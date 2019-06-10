e81e8e2ebaaa3edb9b61d022517f7600394b2aab
Jenna <jemelise@gmail.com>
2019-06-10 18:40:59 -0500

    fixed bug

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -55,7 +55,7 @@ public class ListController {
 
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
         model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
-        model.addAttribute("jobs", jobs);
+        model.addAttribute("results", jobs);
 
         return "list-jobs";
     }
